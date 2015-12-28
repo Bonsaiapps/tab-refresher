2f6e6337107bc10910ab951f434492722d08f651
John Walker <john.walker.dev@gmail.com>
2015-12-27 23:11:11 -0700

    fix(tab-delay)

--- src/popup/timer-popup.js
+++ src/popup/timer-popup.js
@@ -5,9 +5,6 @@
 
 $(() => {
 
-  // chrome.alarms will only deal in minutes
-  // Seemed ok since default was six hours
-  const STATUS_DELAY = 3000
   const COUNTDOWN_DELAY = 1000
 
   class TimerPopup {
@@ -16,19 +13,13 @@ $(() => {
 
     tabManager = new TabManager()
 
-    $start = $('#start')
-    $end = $('#end')
-    $save = $('#save')
-    $refresh = $('#refresh')
+    $startRange = $('#start-range')
+    $endRange = $('#end-range')
+    $refreshVal = $('#refresh-val')
     $startAll = $('#start-all')
-    $check = $('#check')
-    $allOff = $('#all-off')
-    $status = $('#status')
-    $countdownBlock = $('#countdown')
-    $startActive = $('#start-active')
-    $disableActive = $('#disable-active')
-    $resetTimer = $('#reset-timer')
-    $disabledText = $('#disabled-text')
+    $disableAll = $('#disable-all')
+    $startReset = $('#start-reset')
+    $intervalVal = $('#interval-val')
 
     constructor () {
       this.bindEvents()
@@ -36,12 +27,9 @@ $(() => {
     }
 
     bindEvents () {
-      this.$save.click(ev => this.onSaveClick())
-      this.$startAll.click(ev => this.onStartAll())
-      this.$check.click(ev => this.getActiveTabAlarmTime())
-      this.$allOff.click(ev => this.onAllOffClick())
-      this.$startActive.click(ev => this.onStartActive())
-      this.$resetTimer.click(ev => this.onResetTimer())
+      this.$startAll.click(ev => this.onStartAllClick())
+      this.$disableAll.click(ev => this.onDisableAllClick())
+      this.$startReset.click(ev => this.onStartResetClick())
     }
 
     setInitialInterval () {
@@ -49,34 +37,22 @@ $(() => {
         .then(tab => this.tabManager.getFromStorage(tab))
         .then(({ start, end }) => {
           d('start', start, end)
-          this.$start.val(start)
-          this.$end.val(end)
+          this.$startRange.val(start)
+          this.$endRange.val(end)
         })
         .then(() => this.startCountdown())
     }
 
     onStartActive () {
       return this.tabManager.startCurrent()
-        .then(() => this.updateStatus('Tab Started'))
         .then(() => this.startCountdown())
 
     }
 
-    onSaveClick () {
-      return this.tabManager.saveRangeForCurrentTab(this.$start.val(), this.$end.val())
-        .then(() => this.updateStatus('Refresh interval saved!'))
-    }
-
-    onStartAll () {
+    onStartAllClick () {
       return this.tabManager.queryAll()
         .then(tabs => this.tabManager.createAll(tabs))
-        .then(() => this.updateStatus('All tabs started!'))
         .then(() => this.startCountdown())
-    }
-
-    updateStatus (statusText) {
-      this.$status.text(statusText)
-      return setTimeout(() => this.$status.text(''), STATUS_DELAY)
     }
 
     startCountdown () {
@@ -85,30 +61,41 @@ $(() => {
 
     getActiveTabAlarmTime () {
       this.tabManager.findActiveAlarm()
+        .then(alarm => this.parseAlarmTime(alarm))
         .then(display => {
 
           if (!display) return this.hideCountdownBlock()
 
-          this.$countdownBlock.css('display', 'block')
-          this.$disabledText.css('display', 'none')
-          this.$refresh.text(display)
+          this.$refreshVal.text(display)
         })
     }
 
+    parseAlarmTime (alarm) {
+      d('active alarm', alarm)
+      if (!alarm) return
+
+      let {scheduledTime, periodInMinutes} = alarm
+      this.$intervalVal.text(`${periodInMinutes}m`)
+      let timespan = countdown(scheduledTime, new Date().getTime(), countdown.HOURS | countdown.MINUTES | countdown.SECONDS)
+      return timespan.toString(2)
+    }
+
+
     hideCountdownBlock () {
       clearInterval(this.intervalId)
-      this.$disabledText.css('display', 'block')
-      return this.$countdownBlock.css('display', 'none')
+      this.$refreshVal.text('')
+      return this.$intervalVal.text('')
     }
 
-    onAllOffClick () {
+    onDisableAllClick () {
       this.tabManager.storage.setter({global: {off: true}})
       this.tabManager.removeAllAlarms()
-        .then(wasCleared => this.updateStatus('All tabs disabled'))
         .then(() => this.hideCountdownBlock())
     }
-
-    onResetTimer () {
+    onSaveClick () {
+      return this.tabManager.saveRangeForCurrentTab(this.$startRange.val(), this.$endRange.val())
+    }
+    onStartResetClick () {
       this.onSaveClick()
         .then(() => this.onStartActive())
     }
