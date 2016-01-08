39c19affb38e154a6cb5e56f2b6def81ab1b91ea
John Walker <john.walker.dev@gmail.com>
2016-01-08 11:20:40 -0700

    feat(popup-timer): clean structure

---
+++ src/popup-timer/timer-startup.js
@@ -0,0 +1,16 @@
+/**
+ * @author john
+ * @version 12/27/15 10:13 PM
+ */
+
+$(() => {
+
+  let popup = new PopupTimer()
+  popup.bindEvents()
+  popup.checkCurrentRefreshTimer()
+    .catch(err => {
+      console.warn(err)
+      popup.clearValues()
+    })
+
+})
